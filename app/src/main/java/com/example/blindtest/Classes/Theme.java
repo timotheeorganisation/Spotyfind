9e243b4c87a457d81db0e577fea47dd77e379c57
blindtest <maxime-certain@hotmail.fr>
2019-02-23 11:24:39 +0100

    test

---
+++ app/src/main/java/com/example/blindtest/Classes/Theme.java
@@ -0,0 +1,42 @@
+package com.example.blindtest.Classes;
+
+import com.j256.ormlite.field.DatabaseField;
+
+import java.util.Date;
+
+public class Theme {
+    @DatabaseField( columnName = "id", generatedId = true)
+    private int id;
+    @DatabaseField
+    private String libelle;
+
+
+    public Theme()
+    {
+    }
+
+    public Theme(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    @Override
+    public String toString() {
+        return libelle;
+    }
+}
---
+++ app/src/main/java/com/example/blindtest/MenuActivity.java
@@ -0,0 +1,58 @@
+package com.example.blindtest;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.blindtest.BDD.Session;
+import com.example.blindtest.BDD.Variables;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private Button btnLogout;
+    private Session session;
+    private TextView ses;
+    private static final String PREFS_NAME = "PreName";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        ses = findViewById(R.id.edtSession);
+        int PRIVATE_MODE =0;
+
+        session = new Session((this));
+
+        if(!session.loggedin())
+        {
+            logout();
+        }
+        btnLogout = (Button)findViewById(R.id.btnLogout);
+
+        SharedPreferences prefs = getApplicationContext()
+                .getSharedPreferences("MyPref", 0);
+        String s = ((Variables) this.getApplication()).getSomeVariable();
+        ses.setText(s);
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout()
+    {
+        session.setLoggedin(false);
+        finish();
+        startActivity(new Intent(MenuActivity.this, LoginActivity.class));
+    }
+}
